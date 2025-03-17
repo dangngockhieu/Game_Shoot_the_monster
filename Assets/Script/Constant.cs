814bcab96acbb852d9b7dc9127b2b5909c7c3984
dangngockhieu <dangngockhieu28082005@gmail.com>
2025-03-17 09:35:12 +0700

    feat: define game tags and layer constants

---
+++ Assets/Script/Constant.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+// Quản lý tên các TAG
+public class Constant
+{
+    public const string PLAYER_TAG = "Player"; // Người chơi
+    public const string ENEMY_TAG = "Enemy"; // Địch
+    public const string PLAYER_BULLET_TAG = "Player Bullet"; // Đạn của người chơi
+    public const string Enemy_BULLET_TAG = "Enemy Bullet"; // Đạn của địch
+    public const string AUDIO_TAG = "Audio"; // Âm thanh
+    public const string METEORITE_TAG = "Meteorite"; // Thiên thạch ( Thiên thạch rơi trong các Level )
+
+}
+
